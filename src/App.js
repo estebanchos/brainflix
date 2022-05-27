b22721af575964d6113279810dc20071cf78cc64
Carlos Ocampo <charlie@carlosocampo.ca>
2022-05-26 22:55:17 -0500

    comp: completed NextVideos structure and styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import VideoCard from './components/VideoCard/VideoCard';
+import NextVideos from './components/NextVideos/NextVideos';
 import videosFullDetails from './data/video-details.json';
 import './App.scss';
 
@@ -8,14 +8,7 @@ class App extends Component {
   
   render() { 
     return (
-      <ul>
-        <VideoCard 
-          id= "84e96018-4022-434e-80bf-000ce4cd12b8"
-          title= "Become A Travel Pro In One Easy Lesson"
-          channel= "Todd Welch"
-          image= "https://i.imgur.com/5qyCZrD.jpg"
-        />
-      </ul>
+      <NextVideos />
     );
   }
 }
---
+++ src/components/NextVideos/NextVideos.js
@@ -0,0 +1,37 @@
+import { Component } from 'react';
+import './NextVideos.scss';
+import videosDetails from '../../data/videos.json';
+import VideoCard from '../VideoCard/VideoCard';
+
+
+class NextVideos extends Component {
+
+    state = {
+        videos: videosDetails
+    }
+
+    render() {
+        const activeVideo = "84e96018-4022-434e-80bf-000ce4cd12b8"; // need to update to prop
+        // const { activeVideo } = this.props
+
+        return (
+            <section className='next-videos'>
+                <h3 className='next-videos__title'>NEXT VIDEOS</h3>
+                <ul className='next-videos__list'>
+                    {this.state.videos.map(video => {
+                        if(video.id !== activeVideo) {
+                            return <VideoCard
+                                id={video.id}
+                                title={video.title}
+                                channel={video.channel}
+                                image={video.image}
+                            />
+                        }
+                    })}
+                </ul>
+            </section>
+        );
+    }
+}
+
+export default NextVideos;
