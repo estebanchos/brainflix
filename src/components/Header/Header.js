9ae9ff7952143356f5db13e699e4ef63768e13fd
Carlos Ocampo <charlie@carlosocampo.ca>
2022-05-29 13:10:01 -0500

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import searchIcon from '../../assets/images/icons/search.svg';
+import logo from '../../assets/images/logo/brainflix-logo.svg';
+
+
+function Header() {
+    return ( 
+
+     );
+}
+
+export default Header;
